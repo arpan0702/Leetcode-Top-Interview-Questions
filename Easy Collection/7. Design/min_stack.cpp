f8c678a93928a6667386db118445a129b5f3431a
Arpan Dutta <allinonearpandutta@gmail.com>
2021-01-07 16:09:30 +0530

    Design Questions

---
+++ Easy Collection/7. Design/min_stack.cpp
@@ -0,0 +1,39 @@
+class MinStack {
+        stack<int> s1;
+        stack<int> s2;
+public:
+    /** initialize your data structure here. */
+    MinStack() {
+    }
+
+    void push(int x) {
+        s1.push(x);
+        if(s2.empty()||getMin()>=x)
+            s2.push(x);
+    }
+
+    void pop() {
+        if(getMin()==top())
+        {
+            s2.pop();
+        }
+        s1.pop();
+    }
+
+    int top() {
+        return s1.top();
+    }
+
+    int getMin() {
+        return s2.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
---
+++ Easy Collection/7. Design/shuffle_an_array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    vector<int> nums;
+public:
+    Solution(vector<int> nums) {
+        this->nums = nums;
+    }
+
+    vector<int> reset() {
+        return nums;
+    }
+
+    vector<int> shuffle() {
+        vector<int> result(nums);
+        for (int i = 0;i < result.size();i++) {
+            int pos = rand()%(result.size()-i);
+            swap(result[i+pos], result[i]);
+        }
+        return result;
+    }
+};
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * vector<int> param_1 = obj->reset();
+ * vector<int> param_2 = obj->shuffle();
+ */
